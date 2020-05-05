3b031bdbb64bc31aef242f1e07ffc28a09de3853
nath54 <cerisara.nathan@gmail.com>
2020-05-05 08:51:01 +0200

    aa

--- liste_textes.js
+++ liste_textes.js
@@ -11,17 +11,22 @@ const liste_textes=[
         [["texte","TEXTES/VICTORHUGO_fonctiondupoete/texte.html"]]},
     {titre:"Le Crapaud",auteur:"Tristan Corbière",date:"1873",etat:"entier",genre:"poème",tpfich:"textevu",liens:
         [["texte","TEXTES/TRISTANDECORBIERE_lecrapaud/texte.html"]]},
-    {titre:"L'albatros",auteur:"Charles Baudelaire",date:null,etat:"entier",genre:"poème",tpfich:"textevu",liens:
-        [["texte","TEXTES/BAUDELAIRE_lalbatros/texte.html"]]},
+    {titre:"Les fleurs du mal",auteur:"Charles Baudelaire",date:null,etat:"entier",genre:"poème",tpfich:"textevu",liens:
+        [["l'albatros","TEXTES/BAUDELAIRE_fleursdumal/albatros.html"],
+        ["l'ennemi","TEXTES/BAUDELAIRE_fleursdumal/lennemi.html"]]},
     {titre:"Utopia",auteur:"Thomas More",date:"1987",etat:"extrait",genre:"roman",tpfich:"textevu",liens:
         [["texte","TEXTES/THOMASMORE_extrait_dutopia/texte.html"]]},
     {titre:"Essais",auteur:"Montaigne",date:"1580-1588",etat:"extrait",genre:"texte argumentatif",tpfich:"textevu",liens:
         [["extrait n°1","TEXTES/MONTAIGNE_Essais_extraits/extrait1.html"],
         ["extrait n°2","TEXTES/MONTAIGNE_Essais_extraits/extrait2.html"],
+        ["texte d'étude linéaire","TEXTES/MONTAIGNE_Essais_extraits/etlin1.html"],
         ]},
-    //{titre:"",auteur:"",date:null,etat:"extrait",genre:null,tpfich:"textevu",liens:[]},
-    //{titre:"",auteur:"",date:null,etat:"extrait",genre:null,tpfich:"textevu",liens:[]},
-    //{titre:"",auteur:"",date:null,etat:"extrait",genre:null,tpfich:"textevu",liens:[]},
+    {titre:"la découverte des sélénites",auteur:"cyrano de Bergerac",date:"1657",etat:"extrait",genre:"roman",tpfich:"textevu",liens:
+        [["teste d'étude linéaire","TEXTES/CYRANODEBERGERAC_decouvertesdesselenites/et_lin.html"]]},
+    {titre:"Supplément au voyage de Bougainville",auteur:"Denis Diderot",date:1796,etat:"extrait",genre:"roman",tpfich:"textevu",liens:
+        [["texte d'etude linéaire","TEXTES/DEDENISDIDEROT_supplementauvoyagedebougainville/et_lin.html"]]},
+    {titre:"Histoire d'un voyage fait en terre de Brésil",auteur:"Jean de Lery",date:"1578",etat:"extrait",genre:"récit de voyage",tpfich:"textevu",liens:
+        [["texte d'étude linéaire","TEXTES/JEAN_DE_LERY_histoiredunvoiyagefaiteenterredebresil/et_lin.html"]]},
     //{titre:"",auteur:"",date:null,etat:"extrait",genre:null,tpfich:"textevu",liens:[]},
     //{titre:"",auteur:"",date:null,etat:"extrait",genre:null,tpfich:"textevu",liens:[]},
     //{titre:"",auteur:"",date:null,etat:"extrait",genre:null,tpfich:"textevu",liens:[]},
